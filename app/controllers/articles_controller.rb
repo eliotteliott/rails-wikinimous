2a77fd77f62ef257c92352ff9e21253400a70226
Eliott GUERIN <eliott.guerin@gmail.com>
2018-05-15 17:09:27 +0200

    wiki fine tuning

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,12 +4,16 @@ class ArticlesController < ApplicationController
   end
 
   def show          # GET /articles/:id
+    @article = Article.find(params[:id])
   end
 
   def new           # GET /articles/new
+    @article = Article.new
   end
 
   def create        # POST /articles
+    @article = Article.create(article_params)
+    redirect_to article_path(@article)
   end
 
   def edit          # GET /articles/:id/edit
@@ -19,5 +23,14 @@ class ArticlesController < ApplicationController
   end
 
   def destroy       # DELETE /articles/:id
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
   end
+
+  private
+
+  def article_params
+      params.require(:article).permit(:title, :content)
+    end
 end
